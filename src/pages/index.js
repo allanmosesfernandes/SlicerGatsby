598e3b284e3de9bfe465e6f3ebbdba9a36fe50cd
Allan Fernandes <Fernzallan14@gmail.com>
2023-05-29 16:14:16 +0100

    re-init

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import * as React from "react"
+import { HomePageDiv } from "../styles/homepageStyles"
+
+export default function Home() {
+  return <HomePageDiv>Hello world!</HomePageDiv>
+}
---
+++ src/styles/homepageStyles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HomePageDiv = styled.div`
+background-color: red;
+width: 100vw;
+height: 100vh;
+
+`
