316a5f1235f8e1ba4f3bff8a59a984222d7905d2
Allan Fernandes <Fernzallan14@gmail.com>
2023-05-29 23:31:07 +0100

    Global Styles + Typography

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import GlobalStyles from '../styles/GlobalStyles'
+import Typography from '../styles/Typography'
 import Footer from './Footer'
 import Navbar from "./Navbar"
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <>  
+        <GlobalStyles />
+        <Typography />
         <Navbar />
          { children }
         <Footer />
