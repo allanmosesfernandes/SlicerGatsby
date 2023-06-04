19988fa5f68aa38167b18f3a13e82e4adbc0592b
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-04 21:11:05 +0100

    node-code

---
+++ gatsby-node.js
@@ -0,0 +1,39 @@
+const path = require("path")
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+    // 1. Get a template for this page
+    const pizzaTemplate = path.resolve("./src/templates/SinglePizza.js");
+
+    //2. Query all pizzas 
+     const { data } = await graphql(`
+        query MyQuery {
+        Pizzas: allSanityPizza {
+        nodes {
+        name
+        slug {
+            current
+        }
+        }
+    }
+    }`)
+    
+    //3. Loop over each pizza and create a page for that pizza
+    data.Pizzas.nodes.forEach(pizza => {
+        console.log(`pizaa/${pizza.slug.current}`);
+        actions.createPage({
+            path: `pizza/${pizza.slug.current}`,
+            component: pizzaTemplate,
+        })
+    });
+}
+
+
+exports.createPages = async (params) => {
+
+    // Create Pages Dynamically
+    //1.Pizzas
+    await turnPizzasIntoPages(params)
+    //2.Toppings
+    //3.SliceMasters
+
+}
---
+++ src/templates/SinglePizza.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SinglePizza = () => {
+  return (
+    <div>
+        <h2>This is a single pizzas page</h2>
+    </div>
+  )
+}
+
+export default SinglePizza
