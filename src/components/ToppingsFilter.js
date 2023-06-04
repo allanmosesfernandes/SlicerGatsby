d7462eb4f32ca7190083ee764d2b8f7b1cd1ae25
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-04 17:04:20 +0100

    Toppings-Logic

--- src/components/ToppingsFilter.js
+++ src/components/ToppingsFilter.js
@@ -1,4 +1,4 @@
-import { graphql, useStaticQuery } from 'gatsby'
+import { graphql, Link, useStaticQuery } from 'gatsby'
 import React from 'react'
 
 const ToppingsFilter = () => {
@@ -23,11 +23,40 @@ const ToppingsFilter = () => {
       }
     }
   }`)
-  console.clear();
-  console.log({pizzas, toppings});
+
+  //count how many pizzas in toppings
+  function countPizzasInToppings (pizzas) {
+    const ToppingsArray = pizzas.map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+      //check if it is an existing topping
+      const existingTopping = acc[topping.id];
+
+      //if there is an existing topping, then increment by one
+      if(existingTopping) {
+        existingTopping.count += 1;
+      }else {
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        }
+      }
+      return acc
+      //otherwise 
+    }, {});
+
+    //sort pizzas
+    const sortedPizzas = Object.values(ToppingsArray).sort((a,b) => b.count - a.count);
+    return sortedPizzas;
+  }
+  // console.log({pizzas, toppings});
+  const tooppingsWithCount = countPizzasInToppings(pizzas.nodes);
   return (
     <div> 
-      
+        {
+          tooppingsWithCount.map((topping) => (<Link>{topping.name}</Link>))
+        }
     </div>
   )
 }
