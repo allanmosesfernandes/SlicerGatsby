642c6f80c90bbb40e5bcf94b85d1300dbcb5762d
Allan <fernzallan14@gmail.com>
2023-06-13 11:19:39 +0100

    slicers-query

--- src/pages/slicemasters.js
+++ src/pages/slicemasters.js
@@ -1,9 +1,32 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 
-const Slicemasters = () => {
+const Slicemasters = ({ data }) => {
+  
+  const slicersArray = data.allSanityPerson.edges;
+  
   return (
-    <div>Slicemasters</div>
+    <div>
+      
+    </div>
   )
 }
 
 export default Slicemasters
+export const query = graphql`
+query MyQuery {
+  allSanityPerson {
+    edges {
+      node {
+        id
+        name
+        image {
+          asset {
+            gatsbyImage(height: 100)
+          }
+        }
+      }
+    }
+  }
+}
+`
