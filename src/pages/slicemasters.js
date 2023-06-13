3748a806b468fe0a19193bd7a5320a4054faa743
Allan <fernzallan14@gmail.com>
2023-06-13 11:44:04 +0100

    Fetching images

--- src/pages/slicemasters.js
+++ src/pages/slicemasters.js
@@ -1,32 +1,67 @@
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React from 'react'
+import styled from 'styled-components';
 
+
+
+
+const SlicerGrid = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+gap: 2rem;
+.gatsby-image-wrapper {
+  width: 100%;
+}
+img {
+  height: 300px;
+  object-fit: cover;
+  width: 100%;
+}
+`
 const Slicemasters = ({ data }) => {
   
   const slicersArray = data.allSanityPerson.edges;
   
-  return (
-    <div>
-      
-    </div>
-  )
+  return <SlicerGrid>
+    {
+      slicersArray.map((slicer,index) => {
+
+        const { id,name, description } = slicer.node;
+        const image = getImage(slicer.node.image.asset);
+
+        return (
+          <div key={id}>
+            <h2>{name}</h2>
+            <GatsbyImage image={image} alt={name} />
+            <p>{description}</p>
+          </div>
+        );
+      })
+    }
+
+  </SlicerGrid>;
 }
 
 export default Slicemasters
 export const query = graphql`
-query MyQuery {
-  allSanityPerson {
-    edges {
-      node {
-        id
-        name
-        image {
-          asset {
-            gatsbyImage(height: 100)
+  query MyQuery {
+    allSanityPerson {
+      edges {
+        node {
+          id
+          name
+          image {
+            asset {
+              gatsbyImage(layout: CONSTRAINED, width: 400)
+            }
           }
+          slug {
+            current
+          }
+          description
         }
       }
     }
   }
-}
-`
+`;
